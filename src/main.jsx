099e4a81583ee810b4e183afa3d4a6b06b490e2f
southAndy <zatchbell.1206@gmail.com>
2022-11-08 22:34:10 +0800

    feat: adding error-path's solution

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom'
 
-import Root from './routes/root'
+import Root from './routes/root';
+import ErrorPage from './routes/error-page';
 // import App from './App'
 import './index.css'
 
 const routers = createBrowserRouter([{
   path:'/',
-  element:<Root/>
+  element:<Root/>,
+  errorElement:<ErrorPage/>
+  
 }])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
