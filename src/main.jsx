845a0ce7ded8c7073bc39d26fc0f9e903596df0f
southAndy <zatchbell.1206@gmail.com>
2022-11-08 16:36:27 +0800

    feat: import testing routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom'
+
+import Root from './routes/root'
+// import App from './App'
 import './index.css'
+
+const routers = createBrowserRouter([{
+  path:'/',
+  element:<Root/>
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routers}/>
+    {/* <App /> */}
   </React.StrictMode>
 )
---
+++ src/routes/error-page.jsx
@@ -0,0 +1,5 @@
+// import 
+
+// export default function ErrorPage(){
+//     return()
+// }
