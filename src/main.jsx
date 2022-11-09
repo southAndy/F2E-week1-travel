efb62abd9a7616342a997b1af811068c592b5599
southAndy <zatchbell.1206@gmail.com>
2022-11-10 01:15:56 +0800

    feat:create major routes

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,29 @@ import ErrorPage from './routes/error-page';
 // import App from './App'
 import './index.css'
 
-const routers = createBrowserRouter([{
+const routers = createBrowserRouter([
+  {
   path:'/',
   element:<HomePage/>,
-  errorElement:<ErrorPage/>
-  
-}])
+  errorElement:<ErrorPage/> 
+  },
+  {
+    path:'/Detail',
+    element:<section>welcome to detail</section>,
+    errorElement:<ErrorPage/> 
+
+  },
+  {
+    path:'/Result',
+    element:<section>welcome to Result</section>,
+    errorElement:<ErrorPage/> 
+  },
+  {
+    path:'/Favorites',
+    element:<section>welcome to Favorites</section>,
+    errorElement:<ErrorPage/> 
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
