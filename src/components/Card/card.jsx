7133f2b6c4970019e84e29b9b46c7825cb2f5184
southAndy <zatchbell.1206@gmail.com>
2022-11-13 00:14:00 +0800

    feat: update Card's router desgin

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -4,19 +4,19 @@ import icon from "../../assets/images/locationIcon.png"
 
 import '../Card/card.scss'
 
-export default function card({image=null,title='測試標題',locationName='台灣'}){
+export default function card({id=1,image=null,title='測試標題',locationName='台灣'}){
     return (
-        <div className="card-container">
+        <a className="card-container" href={`/detail/${id}`}>
             <div className="card-image">
-                <img src={image} alt="" />
+                <img src={image?.length===0?testPitcture:image} alt="活動圖片" />
             </div>
             <p className="card-title">{title}</p>
             <div className="card-location">
                 <div className="icon-box">
-                    <img src={icon} alt="" />
+                    <img src={icon} alt="座標圖樣" />
                 </div>
                 <span>{locationName}</span>
             </div>
-        </div>
+        </a>
     )
 }
